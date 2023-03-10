4cbe6401d72baea5a3e7e274d662c0f479f7f8ea
Ferneko <fcgraciano@gmail.com>
2023-03-10 20:27:01 -0300

    Criação da classe cliente

---
+++ ConsoleApp_Noite/Cliente.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Noite
+{
+    public class Cliente
+    {
+        private int Id;
+        private string Nome;
+        private string Telefone;
+
+        public Cliente(int id, string nome, string telefone)
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+        }
+        public void setNome(string nome)
+        {
+            if(nome != "")
+            {
+                Nome = nome;
+            }
+        }
+        public string getNome()
+        {
+            return Nome;
+        }
+
+        public int getId()
+        {
+            return Id;
+        }
+
+        public void setId(int id)
+        {
+            Id = id;
+        }
+
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
+    }
+}
