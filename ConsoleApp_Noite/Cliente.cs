dd74cf4127508ff55d0e7aeb970cd6367110f434
fernando.graciano <fcgraciano@gmail.com>
2023-03-17 17:18:28 -0300

    CLasse cliente

---
+++ ConsoleApp_Noite/Cliente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Noite
+{
+    public class Cliente
+    {
+        private int Id;
+        private string Nome;
+        private string Telefone;
+
+        public Cliente(int id, string nome, string telefone)
+        {
+            Id = id;
+            Telefone = telefone;
+            Nome = nome;
+        }
+
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
+
+        public int getId()
+        {
+            return Id;
+        }
+        public void setId(int id)
+        {
+            Id = id;
+        }
+        public void setValor(string nome)
+        {
+            Nome = nome;
+        }
+        public string getValor()
+        {
+            return Nome;
+        }
+    }
+}
