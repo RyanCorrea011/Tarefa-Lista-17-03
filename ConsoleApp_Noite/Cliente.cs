260e896256f93357f485fad3d25e2602ac80b8a6
Ferneko <fcgraciano@gmail.com>
2023-03-17 19:06:12 -0300

    Cliente Endereço

--- ConsoleApp_Noite/Cliente.cs
+++ ConsoleApp_Noite/Cliente.cs
@@ -11,13 +11,29 @@ namespace ConsoleApp_Noite
         private int Id;
         private string Nome;
         private string Telefone;
+        private Endereco Endereco;
 
-        public Cliente(int id, string nome, string telefone)
+        public Cliente(int id,
+                        string nome,
+                            string telefone,
+                                Endereco endereco)
         {
             Id = id;
             Nome = nome;
             Telefone = telefone;
+            Endereco = endereco;
         }
+
+        public void setEndereco(Endereco endereco)
+        {
+            Endereco = endereco;
+        }
+
+        public Endereco getEndereco()
+        {
+            return Endereco;
+        }
+
         public void setNome(string nome)
         {
             if(nome != "")
--- ConsoleApp_Noite/Program.cs
+++ ConsoleApp_Noite/Program.cs
@@ -12,3 +12,26 @@ p1.setValor(11.85M);
 p1.setDescricao("Cerveja Skol 1 litrão");
 
 Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
+
+Endereco endereco = new Endereco(
+    "Rua Mario Miziara",
+    "91",
+    "Sobral",
+    "15906304",
+    "Taquaritinga",
+    "SP");
+
+Cliente c1 = new Cliente(1, "Gabriele", "991340447", endereco);
+Cliente c2 = new Cliente(2, "Gabriela", "991340447",endereco);
+Cliente c3 = new Cliente(3, "Ryan", "991340447", endereco);
+Cliente c4 = new Cliente(4, "Fernanda", "991340447", endereco);
+Cliente c5 = new Cliente(5, "Giovanni", "991340447", endereco);
+
+Console.WriteLine( c2.getEndereco().getRua() + " "+c2.getEndereco().getNumero() );
+
+c2.getEndereco().setNumero("135");
+
+Console.WriteLine(c2.getEndereco().getNumero());
+
+
+
